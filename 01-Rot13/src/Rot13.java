04ff395e85b88e44ee14cb08dcbe2c9c3f5d6c73
Iaas22 <ishaaamin22@gmail.com>
2025-09-24 17:47:11 +0200

    refactorizando funciones

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -6,45 +6,51 @@ public class Rot13 {
             'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
     public static void main(String[] args) {
-        dividirFrase();
+        String frase = "FGH";
+        String Ejemplo = dividirFrase(frase,"cifrar");
+        System.out.println(Ejemplo);
     }
 
-    public static void dividirFrase() {
-        String frase = "ME LLAMO ISHAA";
-        String fraseCifrada = "";
+    public static String dividirFrase(String frase,String modo) {
+        String fraseCambiada = "";
         for (int i = 0; i < frase.length(); i++) {
             char caracter = frase.charAt(i);
-            String cambiado = xifraRot13(caracter, 13);
-            fraseCifrada += cambiado;
+            if (modo.equals("cifrar")) {
+                String cambiado = xifraRot13(caracter, 13);
+            fraseCambiada += cambiado;
+            }
+            else if(modo.equals("descifrar")){
+            String cambiado = desxifraRot13(caracter, 13);
+            fraseCambiada += cambiado;
+            }
         }
-        System.out.println(fraseCifrada);
+        return fraseCambiada;
     }
 
     public static String xifraRot13(char letra, int desplaçament) {
+        char tipoAbecedario[] = abecedario;
         if (Character.isUpperCase(letra)) {
-            for (int i = 0; i < abecedarioMayuscula.length; i++) {
-                if (letra == (abecedarioMayuscula[i])) {
-                    return Character.toString(abecedarioMayuscula[(i + desplaçament) % abecedarioMayuscula.length]);
-                }
-            }
-        } else if (Character.isLowerCase(letra)) {
-            for (int i = 0; i < abecedario.length; i++) {
-                if (letra == (abecedario[i])) {
-                    return Character.toString(abecedario[(i + desplaçament) % abecedario.length]);
-                }
-            }
+            tipoAbecedario = abecedarioMayuscula;
         }
+            for (int i = 0; i < tipoAbecedario.length; i++) {
+                if (letra == (tipoAbecedario[i])) {
+                    return Character.toString(tipoAbecedario[(i + desplaçament) % tipoAbecedario.length]);
+                }
+            }
+         
         return Character.toString(letra);
     }
 
     public static String desxifraRot13(char letra, int desplaçament) {
-        if (Character.isUpperCase(letra)) {
-            for (int i = 0; i < abecedarioMayuscula.length; i++) {
-                if (letra == (abecedarioMayuscula[i])) {
-                    return Character.toString(abecedarioMayuscula[(i - desplaçament+abecedarioMayuscula.length) % abecedarioMayuscula.length]);
+        char tipoAbecedario[] = abecedario;
+        if (Character.isUpperCase(letra)){
+            tipoAbecedario = abecedarioMayuscula;
+        }
+            for (int i = 0; i < tipoAbecedario.length; i++) {
+                if (letra == (tipoAbecedario[i])) {
+                    return Character.toString(tipoAbecedario[(i - desplaçament+tipoAbecedario.length) % tipoAbecedario.length]);
                 }
             }
-        }
         return Character.toString(letra);
     }
 }
