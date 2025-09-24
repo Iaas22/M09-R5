31a31f3c19014401c70e7d15347a9353fd0b8d50
Iaas22 <ishaaamin22@gmail.com>
2025-09-24 17:15:04 +0200

    Funciona cifrado y descifrado

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,18 +1,21 @@
 
 public class Rot13 {
-    static char abecedario[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-    static char abecedarioMayuscula[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    static char abecedario[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    static char abecedarioMayuscula[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
     public static void main(String[] args) {
         dividirFrase();
     }
 
     public static void dividirFrase() {
-        String frase = "Me llamo cristo";
-        String fraseCifrada ="";
+        String frase = "ME LLAMO ISHAA";
+        String fraseCifrada = "";
         for (int i = 0; i < frase.length(); i++) {
             char caracter = frase.charAt(i);
             String cambiado = xifraRot13(caracter, 13);
-            fraseCifrada+=cambiado;
+            fraseCifrada += cambiado;
         }
         System.out.println(fraseCifrada);
     }
@@ -20,17 +23,27 @@ public class Rot13 {
     public static String xifraRot13(char letra, int desplaçament) {
         if (Character.isUpperCase(letra)) {
             for (int i = 0; i < abecedarioMayuscula.length; i++) {
-            if (letra == (abecedarioMayuscula[i])) {
-                return Character.toString(abecedarioMayuscula[(i + desplaçament) % abecedarioMayuscula.length]);
+                if (letra == (abecedarioMayuscula[i])) {
+                    return Character.toString(abecedarioMayuscula[(i + desplaçament) % abecedarioMayuscula.length]);
+                }
             }
-        }
-        }
-        else if(Character.isLowerCase(letra)){
+        } else if (Character.isLowerCase(letra)) {
             for (int i = 0; i < abecedario.length; i++) {
-            if (letra == (abecedario[i])) {
-                return Character.toString(abecedario[(i + desplaçament) % abecedario.length]);
+                if (letra == (abecedario[i])) {
+                    return Character.toString(abecedario[(i + desplaçament) % abecedario.length]);
+                }
             }
         }
+        return Character.toString(letra);
+    }
+
+    public static String desxifraRot13(char letra, int desplaçament) {
+        if (Character.isUpperCase(letra)) {
+            for (int i = 0; i < abecedarioMayuscula.length; i++) {
+                if (letra == (abecedarioMayuscula[i])) {
+                    return Character.toString(abecedarioMayuscula[(i - desplaçament+abecedarioMayuscula.length) % abecedarioMayuscula.length]);
+                }
+            }
         }
         return Character.toString(letra);
     }
