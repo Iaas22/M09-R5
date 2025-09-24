4b4a747047424f3df9dfa6f1a9186ebfcd160abe
Iaas22 <ishaaamin22@gmail.com>
2025-09-24 16:31:31 +0200

    Función que devuelve letra x posiciones desupés

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,12 +1,19 @@
 
 
 public class Rot13 {
-
+    static String abecedario[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
     public static void main(String[] args) {
-        String desxifraRot13 = "Hola";
-
-    String abecedario[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-            for (int i = 0; i < desxifraRot13.length(); i++ ) {
+       String resultado = xifraRot13("a",13);
+       System.out.println(resultado);
     }
-}
-}
+
+    
+    public static String xifraRot13(String letra,int desplaçament){
+        for (int i = 0; i < abecedario.length; i++) {
+            if (letra.equals(abecedario[i])) {
+                return abecedario[(i + desplaçament) % abecedario.length];
+            }
+        }
+        return letra; 
+    }
+} 
