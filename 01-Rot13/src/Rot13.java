487f661a175b56de6ec53ff775a028b2a932597c
Iaas22 <ishaaamin22@gmail.com>
2025-09-24 16:57:26 +0200

    cifrado funciona en minusculas y mausculas

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,19 +1,37 @@
 
-
 public class Rot13 {
-    static String abecedario[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    static char abecedario[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    static char abecedarioMayuscula[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
     public static void main(String[] args) {
-       String resultado = xifraRot13("a",13);
-       System.out.println(resultado);
+        dividirFrase();
     }
 
-    
-    public static String xifraRot13(String letra,int desplaçament){
-        for (int i = 0; i < abecedario.length; i++) {
-            if (letra.equals(abecedario[i])) {
-                return abecedario[(i + desplaçament) % abecedario.length];
+    public static void dividirFrase() {
+        String frase = "Me llamo cristo";
+        String fraseCifrada ="";
+        for (int i = 0; i < frase.length(); i++) {
+            char caracter = frase.charAt(i);
+            String cambiado = xifraRot13(caracter, 13);
+            fraseCifrada+=cambiado;
+        }
+        System.out.println(fraseCifrada);
+    }
+
+    public static String xifraRot13(char letra, int desplaçament) {
+        if (Character.isUpperCase(letra)) {
+            for (int i = 0; i < abecedarioMayuscula.length; i++) {
+            if (letra == (abecedarioMayuscula[i])) {
+                return Character.toString(abecedarioMayuscula[(i + desplaçament) % abecedarioMayuscula.length]);
             }
         }
-        return letra; 
+        }
+        else if(Character.isLowerCase(letra)){
+            for (int i = 0; i < abecedario.length; i++) {
+            if (letra == (abecedario[i])) {
+                return Character.toString(abecedario[(i + desplaçament) % abecedario.length]);
+            }
+        }
+        }
+        return Character.toString(letra);
     }
-} 
+}
