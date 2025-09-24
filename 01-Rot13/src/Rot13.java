7d08a63f9327a7959ea982e970481514e1d462da
Iaas22 <ishaaamin22@gmail.com>
2025-09-24 18:42:58 +0200

    abecedario con caracreres especiales

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -2,10 +2,22 @@ import java.util.Scanner;
 
 public class Rot13 { 
     //arrays de abecedario global
-    static char abecedario[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-    static char abecedarioMayuscula[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    static char[] abecedario = {
+    'a', 'à', 'b', 'c', 'ç', 'd', 'e', 'é', 'è',
+    'f', 'g', 'h', 'i', 'í', 'ï', 'j', 'k', 'l', 'm', 'n', 'ñ',
+    'o', 'ó', 'ò', 'p', 'q', 'r', 's', 't',
+    'u', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z'
+};
+
+
+   static char[] abecedarioMayuscula = {
+    'A', 'À', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È',
+    'F', 'G', 'H', 'I', 'Í', 'Ï', 'J', 'K', 'L', 'M', 'N', 'Ñ',
+    'O', 'Ó', 'Ò', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'Ú', 'Ü', 'V', 'W', 'X', 'Y', 'Z'
+};
+
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -50,6 +62,9 @@ public class Rot13 {
         if (Character.isUpperCase(letra)) {
             tipoAbecedario = abecedarioMayuscula;
         }
+        else if(!Character.isUpperCase(letra)&&!Character.isLowerCase(letra)){
+            return Character.toString(letra);
+        }
         for (int i = 0; i < tipoAbecedario.length; i++) {
             if (letra == (tipoAbecedario[i])) {
                 resultado = Character.toString(tipoAbecedario[(i + desplaçament) % tipoAbecedario.length]);
@@ -63,6 +78,9 @@ public class Rot13 {
         String resultado = "";
         if (Character.isUpperCase(letra)) {
             tipoAbecedario = abecedarioMayuscula;
+        }
+        else if(!Character.isUpperCase(letra)&&!Character.isLowerCase(letra)){
+            return Character.toString(letra);
         }
         for (int i = 0; i < tipoAbecedario.length; i++) {
             if (letra == (tipoAbecedario[i])) {
