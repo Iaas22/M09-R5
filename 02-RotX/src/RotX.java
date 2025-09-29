a82e1273b92234afde056514995b8208b163ef0c
Iaas22 <ishaaamin22@gmail.com>
2025-09-29 19:26:20 +0200

    xifra y desxifra bien (ROT13)

--- 02-RotX/src/RotX.java
+++ 02-RotX/src/RotX.java
@@ -3,31 +3,15 @@ public class RotX  {
     public static char[]abecedarioMinuscula = "aàbcçdeéèfghiíïjklmnñoóòpqrstuúüvwxyz".toCharArray();
     public static char[]abecedarioMayuscula = "AÀBCÇDEÉÈFGHIÍÏJKLMNÑOÓÒPQRSTUÚÜVWXYZ".toCharArray();
 
-
- public static void main(String[] args) {
-     String frase = "Hola, què tal?";
-        String resultado = cambiarFrase(frase, "xifrar");
-        System.out.println("Frase original: " + frase);
-        System.out.println("Frase xifrada: " + resultado);
     
+public static void main(String[] args) {
+    String frase = "Hola, què tal?";
+    String resultadoXifrada = xifraRotX(frase, 13);
+    System.out.println("Frase xifrada: " + resultadoXifrada);
+
+    String resultadoDesxifrada = desxifraRotX(resultadoXifrada, 13);
+    System.out.println("Frase desxifrada: " + resultadoDesxifrada);
 }
-
-
-    public static String cambiarFrase(String frase, String modo) {
-        // usar StringBuilder.append
-        String fraseCambiada = "";
-        for (int i = 0; i < frase.length(); i++) {
-            char caracter = frase.charAt(i);
-            if (modo.equals("xifrar")) {
-                String cambiado = xifraRotX(String.valueOf(caracter), 13);
-                fraseCambiada += cambiado;
-            } else if (modo.equals("desxifrar")) {
-                String cambiado = desxifraRotX(String.valueOf(caracter), 13);
-                fraseCambiada += cambiado;
-            }
-        }
-        return fraseCambiada;
-    }
 
 public static String xifraRotX ( String cadena, int desplaçament) {
     String resultado = "";
@@ -59,10 +43,33 @@ public static String xifraRotX ( String cadena, int desplaçament) {
 
 }
 
-public static String desxifraRotX (String cadena, int desplaçament){
-String resultado = "";
-return resultado;
-
+public static String desxifraRotX (String cadena, int desplaçament) {
+    String resultado = "";
+    for (int i = 0; i < cadena.length(); i++) {
+        char c = cadena.charAt(i);
+        boolean encontrado = false;
+        for (int j = 0; j < abecedarioMinuscula.length; j++) {
+            if (c == abecedarioMinuscula[j]) {
+                int index = (j - desplaçament + abecedarioMinuscula.length) % abecedarioMinuscula.length;
+                resultado += abecedarioMinuscula[index];
+                encontrado = true;
+                break;
+            }
+        }
+        if (encontrado) continue;
+        for (int j = 0; j < abecedarioMayuscula.length; j++) {
+            if (c == abecedarioMayuscula[j]) {
+                int index = (j - desplaçament + abecedarioMayuscula.length) % abecedarioMayuscula.length;
+                resultado += abecedarioMayuscula[index];
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            resultado += c;
+        }
+    }
+    return resultado;
 }
 
 
