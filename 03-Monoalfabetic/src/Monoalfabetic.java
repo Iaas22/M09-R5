3e344038ea0f254dfb7e53ae77d88d180f314bc6
Iaas22 <ishaaamin22@gmail.com>
2025-10-03 17:23:21 +0200

    03-Monoalfabet final de classe

--- 03-Monoalfabetic/src/Monoalfabetic.java
+++ 03-Monoalfabetic/src/Monoalfabetic.java
@@ -4,38 +4,48 @@ import java.util.List;
 
 public class Monoalfabetic {
        public static char[] abecedarioMayuscula = "AÀBCÇDEÉÈFGHIÍÏJKLMNÑOÓÒPQRSTUÚÜVWXYZ".toCharArray();       
+public static char[] abecedarioPermutado = null;
+
 
 public static char[] permutaAlfabet(char[] alfabet) {
-        List<Character> listaCharacters = new ArrayList<Character>
-        ();
-        for (char c : alfabet) {
-            listaCharacters.add(c);
-        }
-        // Imprimir abecedario original
-        System.out.print("Abecedario original: ");
-        for (char c : alfabet) {
-            System.out.print(c);
-        }
-        System.out.println();
+    List<Character> listaCharacters = new ArrayList<Character>();
+    for (char c : alfabet) {
+        listaCharacters.add(c);
+    }
+    // Imprimir abecedario original
+    System.out.print("Abecedario original: ");
+    for (char c : alfabet) {
+        System.out.print(c);
+    }
+    System.out.println();
 
-        // Imprimir abecedario cambiado
-        Collections.shuffle(listaCharacters);
-        System.out.print("Abecedario cambiado: ");
-        for (char c : listaCharacters) {
-            System.out.print(c);
-        }
-        System.out.println();
-        Collections.shuffle(listaCharacters);
-        char[] permutado = new char[alfabet.length];
-        for (int i = 0; i < alfabet.length; i++) {
-            permutado[i] = listaCharacters.get(i);
-        }
-        return permutado;
+    // Imprimir abecedario cambiado
+    Collections.shuffle(listaCharacters);
+    System.out.print("Abecedario permutado: ");
+    for (char c : listaCharacters) {
+        System.out.print(c);
+    }
+    System.out.println();
 
+    char[] permutado = new char[alfabet.length];
+    for (int i = 0; i < alfabet.length; i++) {
+        permutado[i] = listaCharacters.get(i);
+    }
+
+    // Guardar el abecedario permutado en la variable estática
+    abecedarioPermutado = permutado;
+
+    return permutado;
+}
+
+//obtener el abecedario permutado guardado
+public static char[] getAbecedarioPermutado() {
+    return abecedarioPermutado;
 }
 
 
 public static String [] xifraMonoAlfa(String [] cadena) {
+    
 
     return null;
 }
