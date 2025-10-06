e9713c2cfe8f1f79abba8902e2c97f35a7dc88ec
Iaas22 <ishaaamin22@gmail.com>
2025-10-06 19:58:08 +0200

    solo cifra y descifra la primera letra, no la palabra completa, falta arreglar

--- 03-Monoalfabetic/src/Monoalfabetic.java
+++ 03-Monoalfabetic/src/Monoalfabetic.java
@@ -43,28 +43,75 @@ public static char[] getAbecedarioPermutado() {
     return abecedarioPermutado;
 }
 
-
-public static String [] xifraMonoAlfa(String [] cadena) {
-    
-
-    return null;
-}
-
-
-
-
-
-public static char[] desxifraMonoAlfa(char[]cadena){
-
-return null;
-}
-
-
-
-
-
-
-    public static void main(String[] args) {
+public static String[] xifraMonoAlfa(String[] cadena) {
+    if (abecedarioPermutado == null) {
         permutaAlfabet(abecedarioMayuscula);
     }
+    String[] resultat = new String[cadena.length];
+    for (int i = 0; i < cadena.length; i++) {
+        char[] chars = cadena[i].toCharArray();
+        for (int k = 0; k < chars.length; k++) {
+            int idx = -1;
+            for (int j = 0; j < abecedarioMayuscula.length; j++) {
+                if (abecedarioMayuscula[j] == chars[k]) {
+                    idx = j;
+                    break;
+                }
+            }
+            if (idx != -1) {
+                chars[k] = abecedarioPermutado[idx];
+            }
+            // Si no está en el alfabeto, se deja igual
+        }
+        resultat[i] = new String(chars);
+    }
+    return resultat;
+}
+
+
+
+
+
+public static String[] desxifraMonoAlfa(String[] cadena) {
+    if (abecedarioPermutado == null) {
+        permutaAlfabet(abecedarioMayuscula);
+    }
+    String[] resultat = new String[cadena.length];
+    for (int i = 0; i < cadena.length; i++) {
+        char[] chars = cadena[i].toCharArray();
+        for (int k = 0; k < chars.length; k++) {
+            int idx = -1;
+            for (int j = 0; j < abecedarioPermutado.length; j++) {
+                if (abecedarioPermutado[j] == chars[k]) {
+                    idx = j;
+                    break;
+                }
+            }
+            if (idx != -1) {
+                chars[k] = abecedarioMayuscula[idx];
+            }
+            // Si no está en el abecedario permutado, se deja igual
+        }
+        resultat[i] = new String(chars);
+    }
+    return resultat;
+}
+
+
+public static void main(String[] args) {
+    permutaAlfabet(abecedarioMayuscula);
+
+    String[] textos = {"Hola", "Món", "Àlbum", "çÇ"};
+    String[] xifrat = xifraMonoAlfa(textos);
+    System.out.println("Text xifrat:");
+    for (String s : xifrat) {
+        System.out.println(s);
+    }
+
+    String[] desxifrat = desxifraMonoAlfa(xifrat);
+    System.out.println("Text desxifrat:");
+    for (String s : desxifrat) {
+        System.out.println(s);
+    }
+}
 }
